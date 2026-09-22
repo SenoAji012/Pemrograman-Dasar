a6886952c66e47b7a7d3c477d0faf904255ff0db
SenoAji012 <aji269309@student.smkn1kandeman.sch.id>
2026-09-22 08:22:37 +0700

    Add files via upload

---
+++ AritmatikaPembagian/AritmatikaPembagian/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AritmatikaPembagian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mangga, orang, hasil = 0;
+
+            Console.Write("jumlah mangga= ");
+            mangga = int.Parse(Console.ReadLine());
+            Console.Write("jumlah orang= ");
+            orang = int.Parse(Console.ReadLine());
+
+            hasil = mangga / orang;
+
+            Console.WriteLine($"Hasil mangga / orang = {hasil}");
+        }
+    }
+}
---
+++ AritmatikaPenjumlahan/AritmatikaPenjumlahan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AritmatikaPenjumlahan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mangga, apel, hasil = 0;
+
+            Console.Write("mangga = ");
+            mangga = int.Parse(Console.ReadLine());
+            Console.Write("appel = ");
+            apel = int.Parse(Console.ReadLine());
+
+            // opersasi penjumlahan dengan operator +
+            hasil = mangga + apel;
+
+            Console.WriteLine($"Hasil mangga + appel = {hasil}");
+        }
+    }
+}
